edb73bf50cbe03629259a79043f211bd9b58d72b
Yassir <yassir.chrifialaoui13@gmail.com>
2024-05-13 20:10:19 +0100

    Reorganized everything

---
+++ yassio/build/CMakeFiles/SimpleClient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleClient.dir/src/SimpleClient.cpp.o"
+  "CMakeFiles/SimpleClient.dir/src/SimpleClient.cpp.o.d"
+  "SimpleClient"
+  "SimpleClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ yassio/build/CMakeFiles/SimpleServer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleServer.dir/src/SimpleServer.cpp.o"
+  "CMakeFiles/SimpleServer.dir/src/SimpleServer.cpp.o.d"
+  "SimpleServer"
+  "SimpleServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
